f7a0ecea8fb75751fa30e444713f8503a7f37e95
Evelyne Munguti <evelynekyamwai@gmail.com>
2026-06-04 08:55:09 +0300

    feat: improve the navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,27 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Home from "./Pages/Home";
+import Detector from "./Pages/Detector";
+import VerifyNumber from "./Pages/VerifyNumber";
+import VerifyWebsite from "./Pages/VerifyWebsite";
+import Academy from "./Pages/Academy";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
 
 function App() {
-  return <Home />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/detector" element={<Detector />} />
+        <Route path="/verify-number" element={<VerifyNumber />} />
+        <Route path="/verify-website" element={<VerifyWebsite />} />
+        <Route path="/academy" element={<Academy />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- frontend/src/Pages/Navbar.jsx
+++ frontend/src/Pages/Navbar.jsx
@@ -8,11 +8,18 @@ export default function Navbar() {
       </div>
 
       <ul className="nav-links">
-        <li><a href="/">Home</a></li>
+        {/* <li><a href="/">Home</a></li>
         <li><a href="/">Detector</a></li>
         <li><a href="/">Verify</a></li>
         <li><a href="/">Academy</a></li>
-        <li><a href="/">Report Scam</a></li>
+        <li><a href="/">Report Scam</a></li> */}
+        import { Link } from "react-router-dom";
+
+<Link to="/">Home</Link>
+<Link to="/detector">Detector</Link>
+<Link to="/verify-number">Verify Number</Link>
+<Link to="/verify-website">Verify Website</Link>
+<Link to="/academy">Academy</Link>
       </ul>
 
       <div className="nav-buttons">
